532d4615cfd7dcab21b517d34f7213c483b2c763
Douaa Azegoune <douaa.azegoune.jpac@gmail.com>
2022-04-11 21:06:27 +0200

    Douaa

--- ProyectoPreguntas/Form1.cs
+++ ProyectoPreguntas/Form1.cs
@@ -16,5 +16,10 @@ namespace ProyectoPreguntas
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+   
+        }
     }
 }
