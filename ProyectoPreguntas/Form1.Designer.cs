c07fd1105eeb4e74b906edf86d936f4cae77e789
Douaa Azegoune <douaa.azegoune.jpac@gmail.com>
2022-04-12 19:41:15 +0200

    CommitDouaa

--- ProyectoPreguntas/Form1.Designer.cs
+++ ProyectoPreguntas/Form1.Designer.cs
@@ -45,19 +45,18 @@ namespace ProyectoPreguntas
             // 
             this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.progressBar1.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.progressBar1.Location = new System.Drawing.Point(145, 304);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Location = new System.Drawing.Point(193, 374);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(272, 22);
+            this.progressBar1.Size = new System.Drawing.Size(363, 27);
             this.progressBar1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(275, 340);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(367, 418);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.Size = new System.Drawing.Size(28, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "0%";
             // 
@@ -72,9 +71,10 @@ namespace ProyectoPreguntas
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Miriam Mono CLM", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(154, 60);
+            this.label2.Location = new System.Drawing.Point(205, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(248, 39);
+            this.label2.Size = new System.Drawing.Size(308, 49);
             this.label2.TabIndex = 3;
             this.label2.Text = "Bienvenidos";
             // 
@@ -83,43 +83,47 @@ namespace ProyectoPreguntas
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(217, 99);
+            this.label3.Location = new System.Drawing.Point(289, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 21);
+            this.label3.Size = new System.Drawing.Size(150, 27);
             this.label3.TabIndex = 4;
             this.label3.Text = "Splash Screen";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(142, 273);
+            this.label4.Location = new System.Drawing.Point(189, 336);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.Size = new System.Drawing.Size(69, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Starting...";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(197, 150);
+            this.pictureBox1.Location = new System.Drawing.Point(259, 173);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(151, 91);
+            this.pictureBox1.Size = new System.Drawing.Size(215, 129);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(587, 379);
+            this.ClientSize = new System.Drawing.Size(783, 466);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Splash Screen";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
